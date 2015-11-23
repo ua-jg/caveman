36a96e94651cc71a35cd2290339821192067a493
ua-jg <granjo@2009.local>
2015-11-23 15:03:34 +0100

    Add template file for Venues Store tests

---
+++ src/__tests__/store/Venues.js
@@ -0,0 +1,36 @@
+jest.dontMock('../../stores/Venues');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+const Store = require('../../stores/Venues');
+
+describe('Venues Store', () => {
+
+  describe('Given no data', () => {
+
+    describe('getVenue', () => {
+
+      it('returns empty object', () => {
+
+      });
+
+    });
+
+  });
+
+  describe('Given some venues', () => {
+
+    describe('getVenue', () => {
+
+      it('returns empty object', () => {
+
+      });
+      
+    });
+
+  });
+  
+
+});
